79d0d46a4a0c1a3ff1b1ff12916063b1e9377522
Brianna Beharrie <brianna.beharrie@mymona.uwi.edu>
2024-03-07 03:09:20 -0500

    Exercise 3

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ from flask import render_template, request, redirect, url_for, flash, session, a
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from app.models import UserProfile
-from app.forms import LoginForm
+from .forms import LoginForm
 
 
 ###
